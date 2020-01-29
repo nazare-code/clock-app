14c352dae01463f1ecc24e1444485bfb7fcb14b7
patoc <patricio_chavez@hotmail.com>
2020-01-29 09:31:32 -0300

    removed public dupe

--- microservices/public/js/clock.js
+++
@@ -1,9 +0,0 @@
-const getCurrentTime = () => { 
-    setTimeout(function () {
-    const dateInMillisecs = Date.now();
-    const now = new Date (dateInMillisecs).toLocaleTimeString();
-    document.getElementById("clock").innerHTML = now;
-    getCurrentTime();
-    }, 1000);                    
-};
-getCurrentTime();

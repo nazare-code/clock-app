37b08b79d02e39c24f85e81bc33887d55d5c3886
patoc <patricio_chavez@hotmail.com>
2020-02-21 21:23:50 -0300

    openshift

--- openshift/monolithic/index.js
+++ openshift/monolithic/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('info', process.env.RUNNING_ON || 'local');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send(`./index.html`);
+});
+
+app.get('/info', (req, res) => {
+    res.send(`<link rel="stylesheet" type="text/css" href="./css/clock.css">
+	      <p id="clock" class="clock">${app.set('info')}!</p>`);
 });
 
 app.listen(app.get('port'), ()=> {

c1151afc5c43180d07d7973f6f4e0d042fb35bec
patoc <patricio_chavez@hotmail.com>
2020-03-02 22:05:06 -0300

    instructions added

--- pipelines/clock-app/delete-all.sh
+++ pipelines/clock-app/delete-all.sh
@@ -10,7 +10,7 @@ tkn pipelinerun delete clock-app-pipeline-run-1 --force
 kubectl get replicaset | grep clock-app | awk '{print $1}' | xargs kubectl delete replicaset
 kubectl delete deployment clock-app
 kubectl get pods | grep clock-app | awk '{print $1}' | xargs kubectl delete pod
-kubectl delete services svc-clockbroker svc-clockui
+kubectl delete services svc-clock-app
 echo "[TASK 2] Show current resources"
 kubectl get pods
 kubectl get tekton-pipelines

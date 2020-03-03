7eab873cbc705830921cb3be5b854f6243b78a9b
patoc <patricio_chavez@hotmail.com>
2020-03-02 22:16:21 -0300

    role updated

--- pipelines/clock-app/delete-all.sh
+++ pipelines/clock-app/delete-all.sh
@@ -5,7 +5,6 @@ tkn resources delete clock-app-git clock-app-image --force
 tkn task delete build-docker-image-from-git-source --force
 tkn task delete deploy-using-kubectl --force
 tkn pipeline delete clock-app-pipeline --force
-tkn taskrun delete build-docker-image-from-git-source-task-run --force
 tkn pipelinerun delete clock-app-pipeline-run-1 --force
 kubectl get replicaset | grep clock-app | awk '{print $1}' | xargs kubectl delete replicaset
 kubectl delete deployment clock-app
